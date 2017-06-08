b7b91fb2116ac4dd9da4fad4d145066b977be6f3
Yonatan <hyped@protonmail.com>
2017-06-07 21:14:32 -0500

    Update FreezeCmd.java

--- me/yonatanx/FreezePlus/cmds/FreezeCmd.java
+++ me/yonatanx/FreezePlus/cmds/FreezeCmd.java
@@ -16,26 +16,38 @@ import org.bukkit.entity.Player;
 public class FreezeCmd implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender.hasPermission("freezeplus.freeze")){
-            if (strings.length == 1){
-                Player player = Bukkit.getPlayer(strings[0]);
-                FreezeManager fm = FreezePlus.get().getFreezeManager();
-                if (!fm.isFrozen(player)) {
-                    if (!player.hasPermission("freezeplus.freeze.bypass")) {
-                        fm.freezePlayer(player, commandSender instanceof Player ? (Player) commandSender : null);
-                        commandSender.sendMessage(ChatColor.GRAY + "You have froze " + ChatColor.GOLD + player.getName() + ChatColor.GRAY + ".");
-                    }
-                    else commandSender.sendMessage(ChatColor.RED + "This player cannot be frozen.");
-                }
-                else {
-                    fm.unfreezePlayer(player);
-                    commandSender.sendMessage(ChatColor.GRAY + "You have unfroze " + ChatColor.GOLD + player.getName() + ChatColor.GRAY + ".");
-                }
-
-            }
-            else commandSender.sendMessage(ChatColor.RED + getSyntax());
+        if (!commandSender.hasPermission("freezeplus.freeze")){
+            commandSender.sendMessage(ChatColor.RED + "No permission.");
+            return true;
         }
-        else commandSender.sendMessage(ChatColor.RED + "No permission.");
+
+        if (strings.length != 1){
+            commandSender.sendMessage(ChatColor.RED + getSyntax());
+            return true;
+        }
+
+        Player player = Bukkit.getPlayer(strings[0]);
+        FreezeManager fm = FreezePlus.get().getFreezeManager();
+
+        if (player == null){
+            commandSender.sendMessage(ChatColor.RED + "Player could not be found.");
+            return true;
+        }
+
+        if (player.hasPermission("freezeplus.freeze.bypass")){
+            commandSender.sendMessage(ChatColor.RED + "This player cannot be frozen.");
+            return true;
+        }
+
+        if (!fm.isFrozen(player)){
+            fm.freezePlayer(player, commandSender instanceof Player ? (Player) commandSender : null);
+            commandSender.sendMessage(ChatColor.GRAY + "You have froze " + ChatColor.GOLD + player.getName() + ChatColor.GRAY + ".");
+        }
+        else {
+            fm.unfreezePlayer(player);
+            commandSender.sendMessage(ChatColor.GRAY + "You have unfroze " + ChatColor.GOLD + player.getName() + ChatColor.GRAY + ".");
+        }
+
         return true;
     }
 
